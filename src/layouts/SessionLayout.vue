7a8af5a46c80921cbd4f117b14086fb8486b5f8f
afnan-shakeel <afnanshakeel@gmail.com>
2023-09-11 17:53:23 +0530

    dark mode for mobile

--- src/layouts/SessionLayout.vue
+++ src/layouts/SessionLayout.vue
@@ -167,6 +167,13 @@
                   d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
               </svg>
             </button>
+            <button type="button" @click="toggleDarkMode()"
+              class="relative ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+              <span class="absolute -inset-1.5"></span>
+              <span class="sr-only">Dark Mode</span>
+              <img v-if="!darkMode" width="30" height="30" src="/dark_ship.svg" alt="dark-ship"/>
+              <img v-if="darkMode" width="30" height="30" src="/light_ship.svg" alt="light-ship"/>
+            </button>
           </div>
           <div v-if="settingsMenu" class="mt-3 space-y-1 px-2">
             <a href="#"
@@ -198,11 +205,11 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { useRouter, useRoute } from 'vue-router';
+import { useRouter } from 'vue-router';
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
 import { ChevronDownIcon } from '@heroicons/vue/20/solid'
 const router = useRouter()
-const route = useRoute()
+// const route = useRoute()
 const settingsMenu = ref(false)
 const mobileMenu = ref(false)
 const menusList = ref([
@@ -242,7 +249,7 @@ const menusList = ref([
   },
 ])
 
-const pageHeader = ref(route.name)
+// const pageHeader = ref(route.name)
 
 const routeTo = (index: number, to: any, item: any) => {
   menusList.value.forEach((x: any) => x.isActive = false)
@@ -259,7 +266,6 @@ const signOut = () => {
 
 const darkMode = ref(false)
 const toggleDarkMode = () => {
-  const element = document.getElementById('html-root')
   darkMode.value = !darkMode.value
   if (darkMode.value) {
     document.documentElement.classList.add('dark')
