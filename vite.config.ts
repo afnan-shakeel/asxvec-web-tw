92e06310589ded7923e6dd6935a3f19f6332b66d
afnan-shakeel <afnanshakeel@gmail.com>
2023-09-14 18:48:59 +0530

    pwa 2

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,41 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      registerType: 'autoUpdate'
+      registerType: 'autoUpdate',
+      workbox: {
+        cleanupOutdatedCaches: false
+      },
+      manifest: {
+        "name": "asxvec web app",
+        "short_name": "asxvec",
+        "start_url": "/",
+        "display": "standalone",
+        "background_color": "#ffffff",
+        "lang": "en",
+        "scope": "/",
+        "icons":[
+          {
+            src:'icons/icon-192x192.png',
+            size:'192x192',
+            type: 'image/png'
+          },
+          {
+            src:'icons/icon-256x256.png',
+            size:'256x256',
+            type: 'image/png'
+          },
+          {
+            src:'icons/icon-384x384.png',
+            size:'384x384',
+            type: 'image/png'
+          },
+          {
+            src:'icons/icon-512x512.png',
+            size:'512x512',
+            type: 'image/png'
+          },
+        ]
+    }
     })
   ],
 })
