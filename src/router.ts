6e22996ea9bb5e9e70fcc4a35aef7a442b7eab5b
afnan-shakeel <afnanshakeel@gmail.com>
2023-12-02 23:05:44 +0530

    build optimize - component dynamic imports

--- src/router.ts
+++ src/router.ts
@@ -2,13 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import SessionLayout from './layouts/SessionLayout.vue'
 import LoginLayout from './layouts/LoginLayout.vue'
 import Login from './pages/Login.vue'
-import Dashboard from './pages/Dashboard.vue'
-import Profile from './pages/Profile.vue'
-import Form from './pages/Form.vue'
-import User from './pages/User.vue'
-import Blog from './pages/Blog.vue'
-import TimeLine from './pages/TimeLine.vue'
-import PalestineIsrael from './pages/PalestineIsrael.vue'
 
 export default createRouter({
   history: createWebHistory(),
@@ -22,43 +15,43 @@ export default createRouter({
     {
       name: 'Profile',
       path: '/profile',
-      component: Profile,
+      component: () => import('./pages/Profile.vue'),
       meta: { layout: SessionLayout }
     },
     {
       name: 'Dashboard',
       path: '/dashboard',
-      component: Dashboard,
+      component: ()=> import('./pages/Dashboard.vue'),
       meta: { layout: SessionLayout }
     },
     {
       name: 'Form',
       path: '/form',
-      component: Form,
+      component: ()=> import('./pages/Form.vue'),
       meta: { layout: SessionLayout }
     },
     {
       name: 'User',
       path: '/user',
-      component: User,
+      component: () => import('./pages/User.vue'),
       meta: { layout: SessionLayout }
     },
     {
       name: 'Blog',
       path: '/blog',
-      component: Blog,
+      component: ()=> import('./pages/Blog.vue'),
       meta: { layout: SessionLayout }
     },
     {
       name: 'Time-line',
       path: '/time-line',
-      component: TimeLine,
+      component: ()=> import('./pages/TimeLine.vue'),
       meta: { layout: SessionLayout }
     },
     {
       name: 'History Timeline',
       path: '/history-timeline',
-      component: PalestineIsrael,
+      component: ()=> import('./pages/PalestineIsrael.vue'),
       meta: { layout: SessionLayout }
     },
 
