504505ac0798e8d41d74e4a982a2e7ced8afd4fd
afnan-shakeel <afnanshakeel@gmail.com>
2023-12-26 21:17:57 +0530

    profile page - about me

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -59,30 +59,68 @@
                 <div class="hidden sm:mb-8 sm:flex sm:justify-center">
                     <div
                         class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                        Announcing myself, <span class="font-medium">AFNAN SHAKEEL</span>. <a
-                            @click="scrollToSocialContact()" class="font-semibold cursor-pointer text-indigo-600"><span
-                                class="absolute inset-0" aria-hidden="true" />Connect
-                            me! <span aria-hidden="true">&rarr;</span></a>
+                        Announcing myself, <span class="font-medium">AFNAN SHAKEEL</span>. <a @click="scrollToServiceContent()"
+                            class="font-semibold cursor-pointer text-indigo-600"><span class="absolute inset-0"
+                                aria-hidden="true" />My Services! <span aria-hidden="true">&rarr;</span></a>
                     </div>
                 </div>
                 <div class="text-center">
                     <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Here to enrich your business
                         ideas</h1>
                     <p class="mt-6 text-lg leading-8 text-gray-600"> Empower your journey with
-                        tech to break down barriers and ignite possibilities. Let the vision guide the code.</p>
+                        tech to break down barriers and ignite possibilities. </p>
                     <div class="mt-10 flex items-center justify-center gap-x-6">
                         <a href="/contact"
                             class="rounded-md cursor-pointer bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                             Hire Me</a>
-                        <a @click="scrollTo()" class="text-sm cursor-pointer font-semibold leading-6 text-gray-900">Learn
-                            more <span aria-hidden="true">→</span></a>
+                        <a @click="scrollToworkContent()" class="text-sm cursor-pointer font-semibold leading-6 text-gray-900">
+                            My Works! <span aria-hidden="true">→</span></a>
                     </div>
                 </div>
             </div>
-            <div class="flex justify-center lg:col-start-2 lg:row-span-2 lg:row-start-1 ">
+            <!-- <div class="flex justify-center lg:col-start-2 lg:row-span-2 lg:row-start-1 ">
                 <img class="w-[48rem] max-w-full rounded-xl bg-gray-900 shadow-xl ring-8 ring-gray-400/10  "
                     src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png" alt="" />
+            </div> -->
+
+
+            <!-- about me -->
+            <div ref="aboutMeRef">
+                <div class=" mx-auto px-6 py-10">
+                    <div class="mx-auto max-w-4xl grid grid-cols-1 gap-x-8 lg:grid-cols-3 ">
+                        <div class="flex justify-center mb-6">
+                            <img class="w-[24rem] rounded-xl bg-gray-900 shadow-xl ring-8 ring-gray-400/10  "
+                                src="https://asxvec4storage.blob.core.windows.net/blog/profile_pic.jpg" alt="" />
+                        </div>
+                        <div class=" col-span-2">
+                            <blockquote
+                                class="text-center text-sm font-semibold leading-8 text-gray-900 sm:text-xl sm:leading-9">
+                                <p>“I wear two hats - a software developer's logic and a tech enthusiast's spark. Armed with
+                                    a
+                                    post-graduate degree and 1.5 years of experience, I dive into the depth of cloud and
+                                    master a spectrum of technologies into ,
+                                    developing and building bridges between ideas and meaningful business products and
+                                    solutions. Journey to a brighter future!”</p>
+                            </blockquote>
+                            <figcaption class="mt-5">
+                                <div class="flex justify-center">
+                                    <social-media-handlers></social-media-handlers>
+                                </div>
+                                <!-- <img class="mx-auto h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""> -->
+                                <div class="mt-4 flex items-center justify-center space-x-3 text-base">
+                                    <div class="font-semibold text-gray-900">Afnan Shakeel</div>
+                                    <svg viewBox="0 0 2 2" width="3" height="3" aria-hidden="true" class="fill-gray-900">
+                                        <circle cx="1" cy="1" r="1" />
+                                    </svg>
+                                    <div class="text-gray-600">Developer</div>
+                                </div>
+                            </figcaption>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <!-- about me -->
+
             <!-- services -->
             <div class="" ref="serviceContentRef">
                 <div
@@ -120,7 +158,23 @@
                     </div>
                 </div>
             </div>
+            <!-- services -->
 
+            <!-- work/projects -->
+            <div class="" ref="workContentRef">
+                <div
+                    class="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+                    <div>
+                        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Work Projects
+                        </h2>
+                        <p class="mt-4 text-gray-500">IN DEVELOPMENT. COMING SOON!
+                        </p>
+
+                    </div>
+
+                </div>
+            </div>
+            <!-- work/projects -->
 
             <footer ref="socialContactRef" class="relative z-10   pt-20 pb-10 lg:pt-[120px] lg:pb-5">
                 <div class="container mx-auto">
@@ -181,37 +235,7 @@
                             </div>
                             <div class="w-full px-4 sm:w-1/2 lg:w-3/12">
                                 <div class="w-full mb-10">
-                                    <div class="flex items-center mb-6">
-                                        <!-- insatgram -->
-                                        <a href="javascript:void(0)"
-                                            class="flex items-center justify-center w-8 h-8 mr-3 rounded-full text-dark dark:border-dark-3  dark:text-white sm:mr-4 lg:mr-3 xl:mr-4">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="currentColor"
-                                                viewBox="0 0 24 24">
-                                                <path
-                                                    d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
-                                            </svg>
-                                        </a>
-                                        <!-- linkedIn -->
-                                        <a href="https://linkedin.com/in/afnan-shakeel/"
-                                            class="flex items-center justify-center w-8 h-8 mr-3  rounded-full text-dark dark:border-dark-3  dark:text-white sm:mr-4 lg:mr-3 xl:mr-4">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="currentColor"
-                                                viewBox="0 0 24 24">
-                                                <path
-                                                    d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
-                                            </svg>
-                                        </a>
-                                        <!-- github -->
-                                        <a href="https://github.com/afnan-shakeel"
-                                            class="flex items-center justify-center w-8 h-8 mr-3  rounded-full text-dark dark:border-dark-3  dark:text-white sm:mr-4 lg:mr-3 xl:mr-4">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="currentColor"
-                                                viewBox="0 0 24 24">
-                                                <path
-                                                    d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                                            </svg>
-                                        </a>
-
-                                    </div>
-
+                                    <social-media-handlers></social-media-handlers>
                                 </div>
                             </div>
                         </div>
@@ -237,7 +261,7 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-
+import SocialMediaHandlers from '../components/SocialMediaHandlers.vue';
 
 
 const features = [
@@ -248,13 +272,22 @@ const features = [
     { name: 'Cloud Services', description: 'Use of various cloud services from AWS, Azure and Google. Developing Image to Text Analysis tools and Speech to Text or Text to Speech (TTS and STT) tools.' },
     { name: 'Host/Deploy', description: 'Firebase deployment and hosting for small applications. Amazon EC2 for heavy applications and for easy project management' },
 ]
+
 // const contentImage =  ref('https://asxvec4storage.blob.core.windows.net/blog/profile_pic.jpg')
-const socialContactRef = ref()
-function scrollTo() {
+const serviceContentRef = ref()
+function scrollToServiceContent() {
     serviceContentRef.value.scrollIntoView({ behavior: 'smooth' })
 }
-const serviceContentRef = ref()
-function scrollToSocialContact() {
-    socialContactRef.value.scrollIntoView({ behavior: 'smooth' })
+const workContentRef = ref()
+function scrollToworkContent() {
+    workContentRef.value.scrollIntoView({ behavior: 'smooth' })
 }
+const socialContactRef = ref()
+// function scrollToSocialContact() {
+//     socialContactRef.value.scrollIntoView({ behavior: 'smooth' })
+// }
+const aboutMeRef = ref()
+// function scrollToaboutMe() {
+//     aboutMeRef.value.scrollIntoView({ behavior: 'smooth' })
+// }
 </script>
